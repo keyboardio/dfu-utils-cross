cc71e615b01448e2bde62dee8ded8e2e8c636056
Jesse Vincent <jesse@keyboard.io>
2022-06-01 14:30:31 -0700

    Refactor the linux compilation script to remove repeated code

--- compile_linux.sh
+++ compile_linux.sh
@@ -15,58 +15,34 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-mkdir -p distrib/linux64
-cd libusb
-export LIBUSB_DIR=`pwd`
-CFLAGS="-lrt" ./configure --enable-static --disable-shared --disable-udev --host=x86_64-ubuntu16.04-linux-gnu
-make clean
-make
-cd ..
-cd dfu-util
-CFLAGS="-lrt" USB_CFLAGS="-I$LIBUSB_DIR/libusb/" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread" ./configure --host=x86_64-ubuntu16.04-linux-gnu
-make clean
-CFLAGS=-static make
-cp src/dfu-suffix src/dfu-prefix src/dfu-util  ../distrib/linux64/
-cd ..
 
-mkdir -p distrib/linux32
-cd libusb
-export LIBUSB_DIR=`pwd`
-CFLAGS="-lrt" ./configure  --enable-static --disable-shared --disable-udev --host=i686-ubuntu16.04-linux-gnu
-make clean
-make
-cd ..
-cd dfu-util
-CFLAGS="-lrt" USB_CFLAGS="-I$LIBUSB_DIR/libusb/" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread" ./configure --host=i686-ubuntu16.04-linux-gnu
-make clean
-CFLAGS=-static make
-cp src/dfu-suffix src/dfu-prefix src/dfu-util ../distrib/linux32
-cd ..
+# Intel builds want -lrt. ARM builds don't
+export CFLAGS="-lrt"
+build_linux "distrib/linux64" "x86_64-ubuntu16.04-linux-gnu"
+build_linux "distrib/linux32" "i686-ubuntu16.04-linux-gnu"
+export CFLAGS=""
 
-mkdir -p distrib/arm
-cd libusb
-export LIBUSB_DIR=`pwd`
-./configure --enable-static --disable-shared --disable-udev --host=arm-linux-gnueabihf
-make clean
-make
-cd ..
-cd dfu-util
-USB_CFLAGS="-I$LIBUSB_DIR/libusb/" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread" ./configure --host=arm-linux-gnueabihf
-make clean
-CFLAGS=-static make
-cp src/dfu-suffix src/dfu-prefix src/dfu-util ../distrib/arm/
-cd ..
+build_linux "distrib/arm" "arm-linux-gnueabihf"
+build_linux "distrib/arm64" "aarch64-linux-gnu"
 
-mkdir -p distrib/arm64
-cd libusb
-export LIBUSB_DIR=`pwd`
-./configure --enable-static --disable-shared --disable-udev --host=aarch64-linux-gnu
-make clean
-make
-cd ..
-cd dfu-util
-USB_CFLAGS="-I$LIBUSB_DIR/libusb/" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread" ./configure --host=aarch64-linux-gnu
-make clean
-CFLAGS=-static make
-cp src/dfu-suffix src/dfu-prefix src/dfu-util ../distrib/arm64/
-cd ..
+
+function build_linux () {
+	local dist_dir = "$1"
+	local archname = "$2"
+	
+	mkdir -p $dist_dir
+
+	cd libusb
+	export LIBUSB_DIR=`pwd`
+	./configure --enable-static --disable-shared --disable-udev --host="$arch_name"
+	make clean
+	make
+	cd ..
+
+	cd dfu-util
+	USB_CFLAGS="-I$LIBUSB_DIR/libusb/" USB_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread" ./configure --host="$arch_name"
+	make clean
+	CFLAGS=-static make
+	cp src/dfu-suffix src/dfu-prefix src/dfu-util "../$dist_dir"
+	cd ..
+}
